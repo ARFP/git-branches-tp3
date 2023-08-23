cd29e54fed22881136702a0560cd1b6f7f7434d3
CRM202A <mdevoldere@users.noreply.github.com>
2023-08-23 12:52:02 +0200

    Création du projet Dotnet

---
+++ LoggerApp/Logger/AbstractLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerApp.Logger
+{
+    public abstract class AbstractLogger
+    {
+        public const int INFO = 1;
+        public const int DEBUG = 2;
+        public const int ERROR = 3;
+
+        public abstract void SetNextLogger(AbstractLogger logger);
+        public abstract void LogMessage(string message);
+        protected abstract void Write(string message);
+    }
+}
---
+++ LoggerApp/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Création et décoration de logs");
